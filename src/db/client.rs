2343ae7b343fc584cddb6d462d534b064b137cfe
Liam Davis <davisliam123@gmail.com>
2025-10-23 17:49:15 -0400

    Added nightly rust chain to remove unimplemented functions from coverage

--- src/db/client.rs
+++ src/db/client.rs
@@ -2,6 +2,7 @@ use indradb::{Database, RocksdbDatastore};
 use std::path::Path;
 
 /// Initialize RocksDB-backed IndraDB database
+#[coverage(off)]
 pub fn init_datastore(path: &Path) -> Result<Database<RocksdbDatastore>, indradb::Error> {
     RocksdbDatastore::new_db(path)
 }
